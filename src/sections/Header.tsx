587a78a05a8d2668028c69a2dd0f287ad3f205d7
SophieRost <sophie.rost13@gmail.com>
2024-05-24 09:07:09 +0100

    add css file for grid edit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,21 +1,11 @@
 import logo from '../assets/logo.svg';
+import ImageGrid from '../components/ImageGrid';
 
 function Header() {
   return (
     <div className="Header">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ImageGrid/>
       </header>
     </div>
   );
